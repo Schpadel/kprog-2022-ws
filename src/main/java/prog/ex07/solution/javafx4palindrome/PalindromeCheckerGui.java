8f355fa42c114d7640381ef718222a6547b62403
Schpadel <sandmann.patrick@yahoo.de>
2022-11-03 14:06:18 +0100

    GUI Solution from TEMP directory

--- src/main/java/prog/ex07/solution/javafx4palindrome/PalindromeCheckerGui.java
+++ src/main/java/prog/ex07/solution/javafx4palindrome/PalindromeCheckerGui.java
@@ -1,5 +1,9 @@
 package prog.ex07.solution.javafx4palindrome;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.FlowPane;
 import prog.ex07.exercise.javafx4palindrome.PalindromeChecker;
 
@@ -10,6 +14,36 @@ public class PalindromeCheckerGui extends FlowPane {
   private static final org.slf4j.Logger logger =
           org.slf4j.LoggerFactory.getLogger(PalindromeCheckerGui.class);
 
+  static final String SUCCESS = "SUCCESS";
+  static final String FAILURE = "FAILURE";
+  TextField palindromeInput = new TextField("Please enter your text!");
+  Label result = new Label();
+  Button button = new Button("Check Palindrome!");
+
+  /**
+   * Create a new Palindrome Checker Gui.
+   *
+   * @param palindromeChecker checker to be used for GUI
+   */
   public PalindromeCheckerGui(PalindromeChecker palindromeChecker) {
+
+    palindromeInput.setOnKeyPressed((event) -> {
+      if (event.getCode().equals(KeyCode.ENTER)) {
+        evaluateInputPalindrome(palindromeChecker);
+      }
+    });
+    this.getChildren().add(palindromeInput);
+    this.getChildren().add(button);
+    this.getChildren().add(result);
+    button.setOnAction((event) -> {
+      evaluateInputPalindrome(palindromeChecker);
+      event.consume();
+    });
+  }
+
+  private void evaluateInputPalindrome(PalindromeChecker palindromeChecker) {
+    String resultText = palindromeChecker.isPalindrome(palindromeInput.getText()) ? SUCCESS :
+        FAILURE;
+    result.setText(resultText);
   }
 }
--- src/main/java/prog/ex07/solution/javafx4palindrome/SimplePalindromeChecker.java
+++ src/main/java/prog/ex07/solution/javafx4palindrome/SimplePalindromeChecker.java
@@ -1,5 +1,7 @@
 package prog.ex07.solution.javafx4palindrome;
 
+import java.util.ArrayList;
+import java.util.List;
 import prog.ex07.exercise.javafx4palindrome.PalindromeChecker;
 
 /**
@@ -8,11 +10,36 @@ import prog.ex07.exercise.javafx4palindrome.PalindromeChecker;
 public class SimplePalindromeChecker implements PalindromeChecker {
   @Override
   public boolean isPalindrome(final String line) {
-    return false;
+    char[] normalizedLine = normalizeLine(line);
+
+    int reverseCounter = normalizedLine.length - 1;
+    for (int i = 0; i < normalizedLine.length; i++) {
+      if (normalizedLine[i] != normalizedLine[reverseCounter]) {
+        return false;
+      }
+      reverseCounter--;
+    }
+    return true;
   }
 
   @Override
   public char[] normalizeLine(final String line) {
-    return null;
+    String temp;
+    List<Character> resultList = new ArrayList<>();
+    temp = line.replace(" ", "");
+    temp = temp.toLowerCase();
+    for (char current : temp.toCharArray()) {
+      if (Character.isAlphabetic(current) || Character.isDigit(current)) {
+        resultList.add(current);
+      }
+    }
+
+    char[] resultArray = new char[resultList.size()];
+
+    for (int i = 0; i < resultList.size(); i++) {
+      resultArray[i] = resultList.get(i);
+    }
+
+    return resultArray;
   }
 }
